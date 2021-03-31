fa1a29268fac31daffcc8528cc58397e02d8aac6
Miguel Ortega <mortega0014@gmail.com>
2021-03-30 19:53:42 -0700

    added all of the drill files from class

---
+++ 01_Stu_Python_Code_Drills/Unsolved/07-functions-01/Unsolved/functions-01.py
@@ -0,0 +1,41 @@
+# Define a function `having_fun` that prints "Functions are FUN!".
+# YOUR CODE HERE!
+def having_fun():
+    print('Functions are Fun')
+
+having_fun()
+
+# Define a function `thirty_seven` that prints the sum of 18 and 19.
+# YOUR CODE HERE!
+def thirty_seven(num1, num2):
+    sum = num1 + num2
+    return sum
+
+thirty_seven(18, 19)
+
+# Call the two functions you've defined so far.
+# YOUR CODE HERE!
+# YOUR CODE HERE!
+
+# Define a function `hello` that takes in a string parameter and prints the parameter variable.
+# YOUR CODE HERE!
+
+
+
+# Call your `hello` function.
+# YOUR CODE HERE!
+
+# Define a function `average` that calculates the average between two parameters and returns the average.
+# YOUR CODE HERE!
+def average(num1, num2):
+    avg_formula = (num1 + num2) / 2
+    return avg_formula
+    
+
+# Call the `average` function and assign to a variable `calculated_average`.
+# Print your `calculated_average`.
+# YOUR CODE HERE!
+# YOUR CODE HERE!
+calculated_average = average
+
+print (calculated_average)
